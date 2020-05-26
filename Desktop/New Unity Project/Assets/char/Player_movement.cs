7dd4b5556ca883b066306f8f14a65a7eeaa0483f
Graeme Norris <gnorris@shawnigan.ca>
2020-05-26 15:27:00 -0700

    player movement script

---
+++ Desktop/New Unity Project/Assets/char/Player_movement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 5;
+    private float moveX;
+    public Animator animator;
+    public GameObject ground;
+    // Update is called once per frame
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement ()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump ();
+        }
+
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        if (moveX == 0)
+        {
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            animator.SetBool("isRunning", true);
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        }
+        
+    }
+
+    void Jump ()
+    {
+        Debug.Log("we are now jumping");
+        animator.SetTrigger("takeoff");
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetBool("isJumping", false);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animator.SetBool("isJumping", true);
+    }
+}
